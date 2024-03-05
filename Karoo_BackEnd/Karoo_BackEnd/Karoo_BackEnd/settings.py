aaf60cf6d1f3e07ae9bbfe836435c7d7b6c5486f
kianiebrahim <ebimp4@gmail.com>
2024-03-05 21:39:55 +0330

    new email, documents are added in project                                                                                                      now project is ready to make account by front-end

--- Karoo_BackEnd/Karoo_BackEnd/Karoo_BackEnd/settings.py
+++ Karoo_BackEnd/Karoo_BackEnd/Karoo_BackEnd/settings.py
@@ -155,8 +155,8 @@ AUTHENTICATION_BACKENDS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dj.ebrahim007@gmail.com'
-EMAIL_HOST_PASSWORD = 'loomgjjdmksmtxra'
+EMAIL_HOST_USER = 'app.karoo@gmail.com'
+EMAIL_HOST_PASSWORD = 'bzxyqsltbcarxsie'
 EMAIL_USE_TLS = True
 
 # Setting for enable CORS (access to front-end framework)
