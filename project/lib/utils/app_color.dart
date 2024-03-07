0f377dfc1fefba6184c120588624f71912e73b9d
hhaa1382 <mehranfarhamid82@gmail.com>
2024-03-07 18:10:46 +0330

    app colors add

---
+++ project/lib/utils/app_color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColor{
+  static const Color main = Color(0xffA259FF);
+  static const Color background = Color(0xffF6F5F5);
+  static const Color button1 = Color(0xff0BCE83);
+  static const Color text1 = Color(0xff44266A);
+  static const Color hint = Color(0xffB0AEAE);
+}
