7f493d0f8d49e258e0b65c3e409b54576726f6af
hhaa1382 <mehranfarhamid82@gmail.com>
2024-03-08 19:35:04 +0330

    user file add

---
+++ project/lib/user/user_file.dart
@@ -0,0 +1,36 @@
+class User{
+  String _fullName;
+  String _email;
+  String _password;
+  String _phoneNumber;
+
+  User(this._fullName , this._email , this._password , this._phoneNumber);
+
+  factory User.fromJson(Map<String , dynamic> json){
+    return User(json["full_name"], "", "", json["phone_number"]);
+  }
+
+  set fullName(String name){
+    _fullName = name;
+  }
+
+  set email(String email){
+    _email = email;
+  }
+
+  set phoneNumber(String number){
+    _phoneNumber = number;
+  }
+
+  set password(String password){
+    _password = password;
+  }
+
+  String get fullName{return _fullName;}
+
+  String get email{return _email;}
+
+  String get phoneNumber{return _phoneNumber;}
+
+  String get password{return _password;}
+}
