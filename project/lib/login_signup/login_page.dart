8484d7a61e37f94fc7e42183eba1b81c023e64a5
hhaa1382 <mehranfarhamid82@gmail.com>
2024-03-09 22:44:03 +0330

    login part complete

--- project/lib/login_signup/login_page.dart
+++ project/lib/login_signup/login_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:project/login_signup/request.dart';
 import 'package:project/utils/app_color.dart';
 import 'package:project/widgets/big_text.dart';
 import 'package:project/widgets/custom_text.dart';
 import 'package:project/widgets/text_icon_widget.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage({super.key});
+  const LoginPage({super.key});
 
   @override
   State<StatefulWidget> createState() => _LoginPage();
@@ -16,11 +17,11 @@ class _LoginPage extends State<StatefulWidget>{
   TextEditingController? passwordController = TextEditingController();
   FocusNode emailFocus = FocusNode();
   FocusNode passwordFocus = FocusNode();
-
+  final _formKey = GlobalKey<FormState>();
 
   @override
-  void initState() {
-    super.initState();
+  void dispose() {
+    super.dispose();
     emailController?.dispose();
     passwordController?.dispose();
   }
@@ -49,82 +50,127 @@ class _LoginPage extends State<StatefulWidget>{
           height: screenHeight - kToolbarHeight,
 
           width: double.infinity,
-          child: Container(
-            margin: const EdgeInsets.only(left : 20 , right: 20 ,top: 140),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const BigText(text: "Login"),
-                const SizedBox(height: 10,),
-                const CustomText(
-                    text: "Please sign in to continue.",
-                    size: 20,
-                    textColor: AppColor.text1,
-                    weight: FontWeight.normal),
-                const SizedBox(height: 50,),
-                TextIcon(
-                  labelText: "EMAIL",
-                  icon: Icons.email_outlined,
-                  controller: emailController,
-                ),
-                const SizedBox(height: 30,),
-                TextIcon(
-                  labelText: "PASSWORD",
-                  icon: Icons.email_outlined,
-                  controller: passwordController,
-                ),
-                const SizedBox(height: 40,),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: ElevatedButton(onPressed: (){
-
-                  },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColor.main,
-                      fixedSize: Size(160, 60),
-                    ),
-                    child:const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CustomText(
-                          text: "LOGIN",
-                          size: 20,
-                          textColor: AppColor.background,
-                          weight: FontWeight.bold,
-                        ),
-                        SizedBox(width: 20,),
-                        Icon(Icons.arrow_forward , size: 30 ,color: AppColor.background,)
-                      ],
+          child: Form(
+            key: _formKey,
+            child: Container(
+              margin: const EdgeInsets.only(left : 20 , right: 20 ,top: 100),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const BigText(text: "Login"),
+                  const SizedBox(height: 10,),
+                  const CustomText(
+                      text: "Please sign in to continue.",
+                      size: 20,
+                      textColor: AppColor.text1,
+                      weight: FontWeight.normal),
+                  const SizedBox(height: 60,),
+                  TextIcon(
+                    labelText: "EMAIL",
+                    icon: Icons.email_outlined,
+                    controller: emailController,
+                    validatorFunction: (value){
+                      if(value==null || value.isEmpty){
+                        return "Please enter email";
+                      }
+                      return null;
+                    },
+                    focus: emailFocus,
+                  ),
+                  const SizedBox(height: 30,),
+                  TextIcon(
+                    labelText: "PASSWORD",
+                    icon: Icons.email_outlined,
+                    controller: passwordController,
+                    validatorFunction: (value){
+                      if(value==null || value.isEmpty){
+                        return "Please enter password";
+                      }
+                      return null;
+                    },
+                    focus: passwordFocus,
+                  ),
+                  const SizedBox(height: 40,),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(onPressed: () async {
+                      if(_formKey.currentState!.validate()){
+                        String? email = emailController?.text??"";
+                        String? password = passwordController?.text??"";
+                        try {
+                          var response = await Request.login(
+                              email: email,
+                              password: password);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("You login successfully ."),
+                                duration: Duration(seconds: 3),))
+                              .closed.then((value){
+                            print("access token : $response");
+                          });
+                        }
+                        catch(e){
+                          print(e);
+                          // ScaffoldMessenger.of(context).showSnackBar(
+                          //     SnackBar(content: Text(e.toString()),
+                          //       duration: Duration(seconds: 3),));
+                        }
+                      }
+                      else{
+                        if(emailController?.text==null || emailController?.text==""){
+                          emailFocus.requestFocus();
+                        }
+                        else if(passwordController?.text==null || passwordController?.text==""){
+                          passwordFocus.requestFocus();
+                        }
+                      }
+                    },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColor.main,
+                        fixedSize: Size(160, 60),
+                      ),
+                      child:const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomText(
+                            text: "LOGIN",
+                            size: 20,
+                            textColor: AppColor.background,
+                            weight: FontWeight.bold,
+                          ),
+                          SizedBox(width: 20,),
+                          Icon(Icons.arrow_forward , size: 30 ,color: AppColor.background,)
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 60,),
-                TextButton(onPressed: (){
-
-                },
-                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
-                  child: const CustomText(
-                      text: "Forgot Password?",
-                      size: 20,
-                      textColor: AppColor.loginText1,
-                      weight: FontWeight.normal),
-                ),
-                const SizedBox(height: 15,),
-                Row(children: [
-                  const CustomText(
-                      text: "Don't have an account?",
-                      size: 20,
-                      textColor: AppColor.loginText2,
-                      weight: FontWeight.normal),
-                  const SizedBox(width: 5,),
+                  const SizedBox(height: 60,),
                   TextButton(onPressed: (){
-                  }, child: const CustomText(
-                      text: "Sign up",
-                      size: 20,
-                      textColor: AppColor.loginText1,
-                      weight: FontWeight.normal),)
-                ],)
-              ],
+
+                  },
+                    style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
+                    child: const CustomText(
+                        text: "Forgot Password?",
+                        size: 20,
+                        textColor: AppColor.loginText1,
+                        weight: FontWeight.normal),
+                  ),
+                  const SizedBox(height: 15,),
+                  Row(children: [
+                    const CustomText(
+                        text: "Don't have an account?",
+                        size: 20,
+                        textColor: AppColor.loginText2,
+                        weight: FontWeight.normal),
+                    const SizedBox(width: 5,),
+                    TextButton(onPressed: (){
+                    }, child: const CustomText(
+                        text: "Sign up",
+                        size: 20,
+                        textColor: AppColor.loginText1,
+                        weight: FontWeight.normal),)
+                  ],)
+                ],
+              ),
             ),
           ),
         ),
--- project/lib/login_signup/request.dart
+++ project/lib/login_signup/request.dart
@@ -31,7 +31,7 @@ class Request{
 
   static Future<String> login({required String email,
     required String password}) async{
-    String error = "unable to signup";
+    String error = "unable to login";
     final response = await http.post(Uri.parse(_baseUrl+_loginUrl),
         headers: <String , String>{"Content-Type": "application/json"},
         body:jsonEncode(<String , String>{
@@ -39,8 +39,8 @@ class Request{
           "email": email}
         ));
 
-    if(response.statusCode == 201){
-      return "Please confirm email";
+    if(response.statusCode == 200){
+      return jsonDecode(response.body)["access"];
     }
     else if(response.statusCode == 401){
       error = jsonDecode(response.body)["non_field_errors"][0];
