c90920c585fd79a29e12b9cbc7cb01f2bd7e5212
hhaa1382 <mehranfarhamid82@gmail.com>
2024-03-27 16:03:55 +0430

    edit profile page add

--- project/lib/profile/profile_page.dart
+++ project/lib/profile/profile_page.dart
@@ -13,7 +13,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: ProfilePageAppBar(),
       body: SingleChildScrollView(
         child: Column(children: [
