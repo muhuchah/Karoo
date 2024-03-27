8e9098fed55e5dc9bbc4231b19fdc0c08c4ba2f9
hhaa1382 <mehranfarhamid82@gmail.com>
2024-03-27 15:55:19 +0430

    profile page complete

--- project/lib/profile/profile_page.dart
+++ project/lib/profile/profile_page.dart
@@ -15,18 +15,20 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ProfilePageAppBar(),
-      body: Column(children: [
-        ProfileListTile(title: "Profile",icon: Icons.person_outline,),
-        ProfileListTile(title: "Orders",icon: Icons.ad_units_outlined,),
-        ProfileListTile(title: "Bookmarks",icon: Icons.bookmark_border,),
-        ProfileListTile(title: "Chats",icon: Icons.chat,),
-        SizedBox(height: 40,),
-        ProfileListText(text: "Help"),
-        ProfileListText(text: "Support"),
-        ProfileListText(text: "Karoo FAQ"),
-        ProfileListText(text: "About Karoo"),
-        ProfileListText(text: "Contact us"),
-      ],),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          ProfileListTile(title: "Profile",icon: Icons.person_outline,),
+          ProfileListTile(title: "Orders",icon: Icons.ad_units_outlined,),
+          ProfileListTile(title: "Bookmarks",icon: Icons.bookmark_border,),
+          ProfileListTile(title: "Chats",icon: Icons.chat,),
+          SizedBox(height: 40,),
+          ProfileListText(text: "Help"),
+          ProfileListText(text: "Support"),
+          ProfileListText(text: "Karoo FAQ"),
+          ProfileListText(text: "About Karoo"),
+          ProfileListText(text: "Contact us"),
+        ],),
+      ),
     );
   }
 }
