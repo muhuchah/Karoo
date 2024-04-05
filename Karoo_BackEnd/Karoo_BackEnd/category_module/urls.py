939d1f43a82d845f1f6dae08b8c1e234d68d2e83
kianiebrahim <ebimp4@gmail.com>
2024-04-05 15:06:02 +0330

    API_helper.docx and requierment.txt are modified.

--- Karoo_BackEnd/Karoo_BackEnd/category_module/urls.py
+++ Karoo_BackEnd/Karoo_BackEnd/category_module/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('maincategories/list', views.MainCategoryListAPIView.as_view(), name='maincategory'),
+    path('maincategories/list/', views.MainCategoryListAPIView.as_view(), name='maincategory'),
     path('subcategories/list/', views.SubCategoryListAPIView.as_view(), name='subcategory')
 ]
