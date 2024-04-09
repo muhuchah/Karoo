129d7c9c46ce93ec62eb1ca0aed911cbb9df0b94
hhaa1382 <mehranfarhamid82@gmail.com>
2024-04-10 01:59:48 +0430

    show categories in home page add

--- project/lib/home/home_page.dart
+++ project/lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:project/home/home_page_appbar.dart';
+import 'package:project/home/home_page_search.dart';
 import 'package:project/utils/app_color.dart';
+import 'package:project/widgets/divider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,9 +14,49 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             HomePageSearch(),
+            SizedBox(height: 10,),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  children: getCategories(),
+                ),
+              ),
+            )
           ],
         ),
       ),
     );
+  }
+
+  List<Widget> getCategories(){
+    List<Widget> children = [];
+    List<Widget> rowChildren = [];
+    for(int i = 1 ; i<=8 ; i++){
+      rowChildren.add(Column(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white,
+            ),
+            child: Image.asset("asset/car.jpg"),),
+          Text("Coloring")
+        ],
+      ));
+
+      if(i%4 == 0){
+        children.add(Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children:rowChildren,)
+        );
+        children.add(SizedBox(height: 30,));
+        rowChildren = [];
+      }
+    }
+    children.add(MyDivider());
+    return children;
   }
 }
