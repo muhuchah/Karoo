3e884d04e0449e5db696f9af31d0dda62cfba86d
muhuchah <m.h.chah4@gmail.com>
2024-04-16 11:21:29 +0330

    change project name in Karoo_BackEnd

--- Karoo_BackEnd/Karoo_BackEnd/Karoo_BackEnd/wsgi.py
+++ Karoo_BackEnd/Karoo_BackEnd/Karoo_BackEnd/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_web_project project.
+WSGI config for Karoo_BackEnd project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_web_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Karoo_BackEnd.settings')
 
 application = get_wsgi_application()
