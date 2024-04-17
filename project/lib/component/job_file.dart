a572c2a02c6b61e4420d2a2fdda1e5a3b38e8394
hhaa1382 <mehranfarhamid82@gmail.com>
2024-04-17 15:09:14 +0330

    job class add

---
+++ project/lib/component/job_file.dart
@@ -0,0 +1,52 @@
+class Job{
+  int? _id;
+  String? _title;
+  int? _subCategoryId;
+  String? _subCategory;
+  String? _picture;
+  String? _description;
+
+  set id(int? value) {
+    _id = value;
+  }
+
+  set description(String? value) {
+    _description = value;
+  }
+
+  set picture(String? value) {
+    _picture = value;
+  }
+
+  set subCategory(String? value) {
+    _subCategory = value;
+  }
+
+  set subCategoryId(int? value) {
+    _subCategoryId = value;
+  }
+
+  set title(String? value) {
+    _title = value;
+  }
+
+  int? get subCategoryId => _subCategoryId;
+
+  String? get subCategory => _subCategory;
+
+  String? get picture => _picture;
+
+  String? get title => _title;
+
+  String? get description => _description;
+
+  int? get id => _id;
+}
+
+
+
+// class JobRequest{
+//   static const String _base = "http://192.168.137.1:8000/";
+//   static const String _jobList = "jobs/list/";
+//
+// }
--- project/lib/job/display_job.dart
+++ project/lib/job/display_job.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:project/utils/app_color.dart';
 import 'package:project/widgets/my_appbars.dart';
 
-class DisplayJob extends StatelessWidget {
+class DisplayJobPage extends StatelessWidget {
   String text;
   Function() leadingOnTap;
-  DisplayJob({super.key , required this.text , required this.leadingOnTap});
+  DisplayJobPage({super.key , required this.text , required this.leadingOnTap});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.background,
       appBar: SubAppBar(text: text,leading: leadingOnTap,),
-      body:
+
     );
   }
 }
