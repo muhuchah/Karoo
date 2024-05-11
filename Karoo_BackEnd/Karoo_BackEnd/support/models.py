d111a09238abac8ff618a85d0d8b245f20b3987b
muhuchah <m.h.chah4@gmail.com>
2024-05-11 19:17:35 +0330

    SpamReport model

--- Karoo_BackEnd/Karoo_BackEnd/support/models.py
+++ Karoo_BackEnd/Karoo_BackEnd/support/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from jobs.models import job
+from account_module.models import User
 
-# Create your models here.
+
+class SpamReport(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    job = models.ForeignKey(job, on_delete=models.CASCADE, blank=False, null=False)
+    reported_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"Spam report for job ID: {self.job.id} ({self.reported_at})"
