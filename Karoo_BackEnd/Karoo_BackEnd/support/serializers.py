2e51041be2dbbd8596e1e08e4d2b4941ae5a0675
muhuchah <m.h.chah4@gmail.com>
2024-05-13 11:42:38 +0330

    add ChatRoom View

--- Karoo_BackEnd/Karoo_BackEnd/support/serializers.py
+++ Karoo_BackEnd/Karoo_BackEnd/support/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import SpamReport
+from .models import SpamReport, Message
 
 
 class SpamReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpamReport
         fields = ['message', 'job']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        fields = '__all__'
--- Karoo_BackEnd/Karoo_BackEnd/support/views.py
+++ Karoo_BackEnd/Karoo_BackEnd/support/views.py
@@ -2,9 +2,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 
-from .models import SpamReport
-from .serializers import SpamReportSerializer
+from .models import SpamReport, Message
+from account_module.models import User
+from .serializers import SpamReportSerializer, MessageSerializer
 
 
 class SpamReportView(APIView):
@@ -16,3 +18,23 @@ class SpamReportView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChatRoomView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, recipient_pk):
+        user = request.user
+
+        try:
+            messages = Message.objects.filter(
+                (Q(sender=user) & Q(recipient=recipient_pk)) |
+                (Q(sender=recipient_pk) & Q(recipient=user))
+            )
+
+            serializer = MessageSerializer(messages, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'Recipient not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
