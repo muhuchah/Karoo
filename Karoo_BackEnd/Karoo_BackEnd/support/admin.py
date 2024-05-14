48d31448f7445bf8136fab2d69e957947866eb58
muhuchah <m.h.chah4@gmail.com>
2024-05-14 08:39:56 +0330

    add Case to admin

--- Karoo_BackEnd/Karoo_BackEnd/support/admin.py
+++ Karoo_BackEnd/Karoo_BackEnd/support/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SpamReport, Message
+from .models import SpamReport, Message, Case
 
 admin.site.register(SpamReport)
 admin.site.register(Message)
+admin.site.register(Case)
