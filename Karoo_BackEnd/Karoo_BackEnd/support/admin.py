c6c02af7448ca66d04afad5843fcb04bf78b348d
muhuchah <m.h.chah4@gmail.com>
2024-05-14 09:01:28 +0330

    add Chat and SupportMessage to admin panel

--- Karoo_BackEnd/Karoo_BackEnd/support/admin.py
+++ Karoo_BackEnd/Karoo_BackEnd/support/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import SpamReport, Message, Case
+from .models import SpamReport, Message, Case, Chat, SupportMessage
 
 admin.site.register(SpamReport)
 admin.site.register(Message)
 admin.site.register(Case)
+admin.site.register(Chat)
+admin.site.register(SupportMessage)
