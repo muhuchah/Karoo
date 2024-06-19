61aaf8d01f2a7a095da36b6a03f30a85c8e0c246
muhuchah <m.h.chah4@gmail.com>
2024-06-19 15:10:01 +0330

    retry_on_exception to utils, and load_openai_config

---
+++ Karoo_BackEnd/Karoo_BackEnd/chatbot/utils/utils.py
@@ -0,0 +1,29 @@
+from functools import wraps
+from dotenv import load_dotenv
+import time
+
+def retry_on_exeption(retries=3, delay=1):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            ex = True
+            attempt = 0
+            while ex and attempt < retries:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    attempt += 1
+                    if attempt < retries:
+                        time.sleep(delay)
+                    else:
+                        raise e
+        
+        return wrapper
+    return decorator
+
+
+def load_openai_config():
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    base_url = os.getenv("BASE_URL")
+    return api_key, base_url
