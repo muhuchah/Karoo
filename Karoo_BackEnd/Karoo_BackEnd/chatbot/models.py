8ca6eb70d4ed8e9ca43ecd89147b2367360557f1
muhuchah <m.h.chah4@gmail.com>
2024-06-19 17:18:18 +0330

    update dimention in JobEmbedding

--- Karoo_BackEnd/Karoo_BackEnd/chatbot/models.py
+++ Karoo_BackEnd/Karoo_BackEnd/chatbot/models.py
@@ -7,7 +7,7 @@ from job_module.models import job
 
 class JobEmbedding(models.Model):
     job = models.OneToOneField(job, on_delete=models.CASCADE, related_name='embedding')
-    embedding = VectorField(dimensions=300)
+    embedding = VectorField(dimensions=1536)
 
     def __str__(self):
         return f"Embedding for {self.job.title}"
