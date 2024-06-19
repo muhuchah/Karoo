ea1bd604fa395943a1064311ad135dbf4844e52f
muhuchah <m.h.chah4@gmail.com>
2024-06-19 14:37:25 +0330

    create chatbot app, JobEmbedding model

---
+++ Karoo_BackEnd/Karoo_BackEnd/chatbot/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from pgvector.django import VectorField
+
+from account_module.models import User
+from job_module.models import job
+
+
+class JobEmbedding(models.Model):
+    job = models.OneToOneField(job, on_delete=models.CASCADE, related_name='embedding')
+    embedding = VectorField(dimensions=300)
+
+    def __str__(self):
+        return f"Embedding for {self.job.title}"
