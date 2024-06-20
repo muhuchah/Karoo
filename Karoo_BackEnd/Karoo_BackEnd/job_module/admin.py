d962fd3be10bb96d66fda9321b9256e5b63acf69
muhuchah <m.h.chah4@gmail.com>
2024-06-20 20:52:06 +0330

    delete province and city in admin

--- Karoo_BackEnd/Karoo_BackEnd/job_module/admin.py
+++ Karoo_BackEnd/Karoo_BackEnd/job_module/admin.py
@@ -6,6 +6,3 @@ admin.site.register(models.job)
 admin.site.register(models.job_pictures)
 admin.site.register(models.job_comments)
 admin.site.register(models.skill)
-admin.site.register(models.Province)
-admin.site.register(models.City)
-
